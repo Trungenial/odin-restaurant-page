240e6fbaaea3693ba8c330425be9e451673d4e03
Trungenial <transytrung0752.work@gmail.com>
2025-05-13 21:17:19 +0700

    try fixing

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
+        publicPath: "./",
         filename: "main.js",
         clean: true,
     },
